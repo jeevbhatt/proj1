999a26e82d23ab6ec672ca9058e4ea1d5bd49b5d
jeevbhatt <jeevanbhatt10000@gmail.com>
2025-04-21 21:48:35 +0545

    used prettier formatting

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-const express =require('express');
+const express = require("express");
 const app = express();
 //
-app.get('/',(h,r)=>{
-  r.send("<h1 style='color:orange'>Hare Ram Hare Ram Ram Ram Hare Hare<br> Hare Krishna Hare Krishna Krishna Krishna Hare Hare!</h1>");
+app.get("/", (h, r) => {
+  r.send(
+    "<h1 style='color:orange'>Hare Ram Hare Ram Ram Ram Hare Hare<br> Hare Krishna Hare Krishna Krishna Krishna Hare Hare!</h1>"
+  );
 });
 
-app.get('/about',(req,res)=>{
+app.get("/about", (req, res) => {
   res.send("<a href='https://www.google.com'>About</a>");
 });
 
-app.get("/contact",(c,p)=>{
+app.get("/contact", (c, p) => {
   p.send("<h1 style='color:blue'>Contact</h1>");
 });
 //
-app.listen(3000,function() {
+app.listen(3000, function () {
   console.log(`Server is running on port`);
 });
