9e9223caa0c6418caca6336c133e34aa85591e03
jeevbhatt <jeevanbhatt10000@gmail.com>
2025-05-10 21:34:44 +0545

    created counter

---
+++ frontend/src/About.jsx
@@ -0,0 +1,13 @@
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is the about page.</p>
+      <a href="/">Go to home page </a>
+      <a href="/contact">Go to contact page </a>
+    </div>
+  );
+}
+
+export default About;
